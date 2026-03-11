e46bd59407c7d1fcd1694f9ce229d60128405577
Cyprian <cyprian.chailan@icloud.com>
2026-03-11 17:26:25 +0100

    refactoring 100% coverage

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -8,55 +8,65 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item : items) {
+            updateItem(item);
+        }
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    private void updateItem(Item item) {
+        switch (item.name) {
+            case "Aged Brie" -> updateAgedBrie(item);
+            case "Backstage passes to a TAFKAL80ETC concert" -> updateBackstagePasses(item);
+            case "Sulfuras, Hand of Ragnaros" -> updateSulfuras(item);
+            default -> updateStandardItem(item);
+        }
+    }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+    private void updateAgedBrie(Item item) {
+        increaseQuality(item);
+        item.sellIn--;
+        if (item.sellIn < 0) {
+            increaseQuality(item);
+        }
+    }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+    private void updateBackstagePasses(Item item) {
+        increaseQuality(item);
+        if (item.sellIn <= 10) {
+            increaseQuality(item);
+        }
+        if (item.sellIn <= 5) {
+            increaseQuality(item);
+        }
+        
+        item.sellIn--;
+        
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+    private void updateSulfuras(Item item) {
+        // Sulfuras ne change pas
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+    }
+
+    private void updateStandardItem(Item item) {
+        decreaseQuality(item);
+        item.sellIn--;
+        if (item.sellIn < 0) {
+            decreaseQuality(item);
         }
     }
 }
--- GildedRose/src/main/java/com/gildedrose/Item.java
+++ GildedRose/src/main/java/com/gildedrose/Item.java
@@ -8,12 +8,22 @@ public class Item {
 
     public int quality;
 
+    /**
+     * Constructeur de la classe Item
+     * @param name nom de l'item
+     * @param sellIn nombre de jours restants avant la fin de la vente
+     * @param quality qualite de l'item
+     */
     public Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
 
+    /**
+     * Méthode toString pour afficher l'item
+     * @return chaine de caractere representant l'item
+     */
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
