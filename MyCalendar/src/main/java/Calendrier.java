ef2ba9d54fa19cf80275cca7bddecfd9edcd5c57
Cyprian <cyprian.chailan@icloud.com>
2026-04-24 16:28:09 +0200

    feat: Calendrier.ajouter(evenement)

--- MyCalendar/src/main/java/Calendrier.java
+++ MyCalendar/src/main/java/Calendrier.java
@@ -8,4 +8,8 @@ public final class Calendrier {
     public int taille() {
         return evenements.size();
     }
+
+    public void ajouter(Evenement evenement) {
+        evenements.add(evenement);
+    }
 }
