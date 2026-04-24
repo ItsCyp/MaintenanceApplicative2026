59f8c26eb82526174fc1442344fe72d9068c4105
Cyprian <cyprian.chailan@icloud.com>
2026-04-24 15:06:49 +0200

    feat: TitreEvenement ajout Optional.filter pour eviter que le titre soit vide

--- MyCalendar/src/main/java/TitreEvenement.java
+++ MyCalendar/src/main/java/TitreEvenement.java
@@ -1,9 +1,13 @@
+import java.util.Optional;
+
 public final class TitreEvenement {
 
     private final String valeur;
 
     public TitreEvenement(String valeur) {
-        this.valeur = valeur;
+        this.valeur = Optional.ofNullable(valeur)
+                .filter(v -> !v.isEmpty())
+                .orElseThrow(() -> new IllegalArgumentException("Le titre ne peut pas être vide"));
     }
 
     public String valeur() {
