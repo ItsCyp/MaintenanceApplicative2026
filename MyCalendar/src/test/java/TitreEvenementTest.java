d18f428acfcb874058fbb70abd84acafa4f3b948
Cyprian <cyprian.chailan@icloud.com>
2026-04-24 15:16:53 +0200

    feat(test): TitreEvenement equals et hashCode

--- MyCalendar/src/test/java/TitreEvenementTest.java
+++ MyCalendar/src/test/java/TitreEvenementTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TitreEvenementTest {
@@ -25,5 +26,29 @@ class TitreEvenementTest {
     @Test
     void un_titre_ne_contenant_que_des_espaces_est_refuse() {
         assertThrows(IllegalArgumentException.class, () -> new TitreEvenement("   "));
+    }
+
+    @Test
+    void deux_titres_avec_meme_valeur_sont_egaux() {
+        TitreEvenement titre1 = new TitreEvenement("Réunion équipe");
+        TitreEvenement titre2 = new TitreEvenement("Réunion équipe");
+
+        assertEquals(titre1, titre2);
+    }
+
+    @Test
+    void deux_titres_avec_meme_valeur_ont_meme_hashcode() {
+        TitreEvenement titre1 = new TitreEvenement("Réunion équipe");
+        TitreEvenement titre2 = new TitreEvenement("Réunion équipe");
+
+        assertEquals(titre1.hashCode(), titre2.hashCode());
+    }
+
+    @Test
+    void deux_titres_avec_valeurs_differentes_ne_sont_pas_egaux() {
+        TitreEvenement titre1 = new TitreEvenement("Réunion équipe");
+        TitreEvenement titre2 = new TitreEvenement("Déjeuner");
+
+        assertNotEquals(titre1, titre2);
     }
 }
